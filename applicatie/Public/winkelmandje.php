8f6f8a538d793b3035b8fa2f383bbb2cc33e2eb5
JamiroV03 <jaj.vermeulen@student.han.nl>
2026-01-12 13:36:09 +0100

    Update winkelmandje
    Kleine aanpassingen en toevoeging van het verwijderen per product, het hele winkelmandje legen en het adres dat tijdens het registreren genoteerd is gebruikt wordt voor het afleveradres

--- applicatie/Public/winkelmandje.php
+++ applicatie/Public/winkelmandje.php
@@ -2,54 +2,102 @@
 require_once '../includes/db_connectie.php';
 require_once '../includes/header.php';
 
-if (!isset($_SESSION['winkelmandje'])) {
-    $_SESSION['winkelmandje'] = [];
-}
+$db = maakVerbinding();
 
-// Verwerken van formulier
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['aantal']) && is_array($_POST['aantal'])) {
-
-    foreach ($_POST['aantal'] as $productNaam => $aantal) {
+if (isset($_POST['aantal']) && is_array($_POST['aantal'])) {
+    foreach ($_POST['aantal'] as $product => $aantal) {
         $aantal = (int)$aantal;
 
         if ($aantal > 0) {
-            if (isset($_SESSION['winkelmandje'][$productNaam])) {
-                $_SESSION['winkelmandje'][$productNaam] += $aantal;
-            } else {
-                $_SESSION['winkelmandje'][$productNaam] = $aantal;
-            }
+            $_SESSION['winkelmandje'][$product] = $aantal;
         }
+    }
+}
+
+if (isset($_POST['verwijder_product'])) {
+    $productNaam = $_POST['verwijder_product'];
+
+    if (isset($_SESSION['winkelmandje'][$productNaam])) {
+        unset($_SESSION['winkelmandje'][$productNaam]);
+    }
+}
+
+if (isset($_POST['leeg_winkelmandje'])) {
+    unset($_SESSION['winkelmandje']);
+}
+
+$adres = '';
+
+if (isset($_SESSION['username'])) {
+    $sql = 'SELECT address FROM [User] WHERE username = :username';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([
+        ':username' => $_SESSION['username']
+    ]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user && !empty($user['address'])) {
+        $adres = $user['address'];
     }
 }
 ?>
 
 <h1>Winkelmandje</h1>
-
 <?php if (empty($_SESSION['winkelmandje'])): ?>
     <p>Je winkelmandje is leeg.</p>
 <?php else: ?>
+
     <table border="1" cellpadding="5">
         <tr>
             <th>Product</th>
             <th>Aantal</th>
+            <th>Actie</th>
         </tr>
 
         <?php foreach ($_SESSION['winkelmandje'] as $product => $aantal): ?>
             <tr>
                 <td><?= htmlspecialchars($product) ?></td>
                 <td><?= (int)$aantal ?></td>
+                <td>
+                    <form method="post" style="display:inline">
+                        <button
+                            type="submit"
+                            name="verwijder_product"
+                            value="<?= htmlspecialchars($product) ?>"
+                            title="Verwijder product"
+                        >
+                            X
+                        </button>
+                    </form>
+                </td>
             </tr>
         <?php endforeach; ?>
     </table>
 
     <br>
 
+    <form method="post">
+        <button type="submit" name="leeg_winkelmandje">
+            Leeg winkelmandje
+        </button>
+    </form>
+
+
     <form method="post" action="bestellen.php">
         <label for="adres">Afleveradres:</label><br>
-        <input type="text" name="adres" id="adres" required><br><br>
+        <input
+            type="text"
+            name="adres"
+            id="adres"
+            value="<?= htmlspecialchars($adres) ?>"
+            required
+            style="width:300px;"
+        >
+        <br><br>
 
         <button type="submit">Bestelling plaatsen</button>
     </form>
+
 <?php endif; ?>
 
 <?php
